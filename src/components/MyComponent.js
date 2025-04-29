7d8eec3ca47f35a49582441b5777d775f6e23374
Vaishnavi-veni <vaishnavinr06@gmail.com>
2025-04-29 15:21:11 +0530

    first commmit before changing app.tsx

---
+++ src/components/MyComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const MyComponent = () => {
+  return (
+    <View style={{ padding: 20, backgroundColor: '#f0f0f0' }}>
+      <Text style={{ fontSize: 18, color: 'black' }}>
+        Hello, React Native Developer!
+      </Text>
+    </View>
+  );
+};
+
+export default MyComponent;
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "react-native";
+import MyComponent from "../components/MyComponent";
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <MyComponent/>
+        </View>
+    );
+};
+
+export default HomeScreen;
